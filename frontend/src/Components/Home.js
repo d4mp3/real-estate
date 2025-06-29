c1b85746f87ea1ed002315214556b3d3f8aab325
d4mp3 <damian.pszczola@gmail.com>
2025-06-29 19:15:10 +0200

    style the home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,4 +1,5 @@
 import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import city from "./Assets/city.jpg";
 
 const addPropertyButtonStyle = {
     backgroundColor: "green",
@@ -29,6 +30,33 @@ const rightBoxStyle = {
     marginRight: "10rem"
 };
 
+const cityImg = {
+    width: "100%",
+    height: "92vh",
+};
+
+
+const overlayText = {
+    position: "absolute",
+    zIndex: 100,
+    top: "100px",
+    left: "20px",
+    textAlign: "center",
+}
+
+const homeText = {
+    color: "white",
+    fontWeight: "bold",
+};
+
+const homeBtn = {
+    fontSize: "3.5rem",
+    borderRadius: "15px",
+    backgroundColor: "green",
+    marginTop: "2rem",
+    boxShadow: "3px 3px 3px white",
+};
+
 function Home() {
     return (
         <>
@@ -53,6 +81,16 @@ function Home() {
                     </Box>
                 </Toolbar>
             </AppBar>
+            <div style={{ position: "relative" }}>
+                <Box component="img" src={city} alt="City" sx={cityImg} />
+                <Box component="div" sx={overlayText}>
+                    <Typography variant="h1" sx={homeText}>FIND YOUR <span style={{ color: "green" }}>NEXT PROPERTY</span> ON THE LBREP WEBSITE</Typography>
+                    <Button variant="contained" sx={homeBtn}>
+                        SEE ALL PROPERTIES
+                    </Button>
+                </Box>
+            </div>
+
         </>
     );
 }
