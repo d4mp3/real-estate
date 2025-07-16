f0021d04b0dcf1c99bc7b81f473a79257c477aad
d4mp3 <damian.pszczola@gmail.com>
2025-07-16 17:20:56 +0200

    feat: implement login functionality with useReducer and Axios

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,10 +1,11 @@
+import Axios from "axios";
+import { useEffect, useReducer } from "react";
 import { useNavigate } from "react-router";
 
 // MUI
 import { Button, Grid, TextField, Typography } from "@mui/material";
 
 function Login() {
-  const navigate = useNavigate();
   const formContainer = {
     width: "50%",
     marginLeft: "auto",
@@ -25,9 +26,100 @@ function Login() {
     },
   };
 
+  const navigate = useNavigate();
+
+  const initialState = {
+    usernameValue: "",
+    passwordValue: "",
+    sendRequest: 0,
+    token: null,
+  };
+
+  function ReducerFunction(state, action) {
+    switch (action.type) {
+      case "SET_USERNAME":
+        return { ...state, usernameValue: action.payload };
+      case "SET_PASSWORD":
+        return { ...state, passwordValue: action.payload };
+      case "SET_SEND_REQUEST":
+        return { ...state, sendRequest: action.payload };
+      case "SET_TOKEN":
+        return { ...state, token: action.payload };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(ReducerFunction, initialState);
+
+  function FormSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted");
+    dispatch({ type: "SET_SEND_REQUEST", payload: true });
+  }
+
+  useEffect(() => {
+    if (state.sendRequest) {
+      const source = Axios.CancelToken.source();
+      async function SignIn() {
+        try {
+          const response = await Axios.post(
+            "http://localhost:8000/api-auth-djoser/token/login/",
+            {
+              username: state.usernameValue,
+              password: state.passwordValue,
+            },
+            {
+              cancelToken: source.token,
+            }
+          );
+          console.log(response);
+          dispatch({ type: "SET_TOKEN", payload: response.data.auth_token });
+        //   navigate("/");
+        } catch (error) {
+          console.error("Error submitting form:", error);
+        }
+      }
+      SignIn();
+      return () => {
+        source.cancel();
+      };
+    }
+  }, [state.sendRequest]);
+
+    // Get user info
+    useEffect(() => {
+    if (state.token) {
+      const source = Axios.CancelToken.source();
+      async function GetUserInfo() {
+        try {
+          const response = await Axios.get(
+            "http://localhost:8000/api-auth-djoser/users/me/",
+            {
+              headers: {
+                Authorization: `Token ${state.token}`,
+              },
+            },
+            {
+              cancelToken: source.token,
+            }
+          );
+          console.log(response);
+          navigate("/");
+        } catch (error) {
+          console.error("Error submitting form:", error);
+        }
+      }
+      GetUserInfo();
+      return () => {
+        source.cancel();
+      };
+    }
+  }, [state.token]);
+
   return (
     <div style={formContainer}>
-      <form>
+      <form onSubmit={FormSubmit}>
         <Grid container justifyContent="center">
           <Typography variant="h4" gutterBottom>
             SIGN IN
@@ -39,6 +131,8 @@ function Login() {
             label="Username"
             variant="outlined"
             fullWidth
+            value={state.usernameValue}
+            onChange={(e) => dispatch({ type: "SET_USERNAME", payload: e.target.value })}
           />
         </Grid>
         <Grid container sx={{ marginTop: "1rem" }}>
@@ -48,6 +142,8 @@ function Login() {
             variant="outlined"
             type="password"
             fullWidth
+            value={state.passwordValue}
+            onChange={(e) => dispatch({ type: "SET_PASSWORD", payload: e.target.value })}
           />
         </Grid>
         <Grid container justifyContent="center" sx={{ marginTop: "1rem" }}>
@@ -59,7 +155,12 @@ function Login() {
       <Grid container justifyContent="center" sx={{ marginTop: "1rem" }}>
         <Typography variant="small" gutterBottom>
           Don't have an account yet?{" "}
-          <span onClick={() => navigate("/register")} style={{ cursor: "pointer", color: "green" }}>SIGN UP</span>
+          <span
+            onClick={() => navigate("/register")}
+            style={{ cursor: "pointer", color: "green" }}
+          >
+            SIGN UP
+          </span>
         </Typography>
       </Grid>
     </div>
